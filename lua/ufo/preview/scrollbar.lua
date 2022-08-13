kevinhwang91/nvim-ufo

commit f4805da5c6d123873da87bfe9b05dbd17d9ceb9a

fix(preview): scrollbar shouldn't be filled fully if it's scrollable

lua/ufo/preview/scrollbar.lua
@@ -50,7 +50,7 @@ function ScrollBar:update(topline)
         topline = utils.getWinInfo(self:floatWinid()).topline
     end
     self.topline = topline
-    local barSize = math.ceil(self.height * self.height / self.lineCount)
+    local barSize = math.floor(self.height * self.height / self.lineCount)
 
     local barPos = math.ceil(self.height * topline / self.lineCount)
     if barPos + barSize > self.height then

lua/ufo/preview.lua
@@ -211,11 +211,10 @@ function Preview:peekFoldedLinesUnderCursor(maxHeight, nextLineIncluded, enter)
     if floatwin.bufnr then
         api.nvim_buf_clear_namespace(floatwin.bufnr, self.ns, 0, -1)
     end
-    local lineCount = buf:lineCount()
     if nextLineIncluded ~= false then
-        endLnum = lineCount == endLnum and endLnum or (endLnum + 1)
+        endLnum = buf:lineCount() == endLnum and endLnum or (endLnum + 1)
     end
-    local text = bufmanager:get(curBufnr):lines(lnum, endLnum)
+    local text = buf:lines(lnum, endLnum)
     local height = math.min(#text, maxHeight or 20)
     floatwin:display(api.nvim_get_current_win(), height, text, enter)
     utils.winCall(floatwin.winid, function()

commit e5d5843a285991c196a58a39bb98ea3c379325eb

fix(preview): wrong scrollbar at bottom
Caused by f4805da

lua/ufo/preview/scrollbar.lua
@@ -50,7 +50,10 @@ function ScrollBar:update(topline)
         topline = utils.getWinInfo(self:floatWinid()).topline
     end
     self.topline = topline
-    local barSize = math.max(1, math.floor(self.height * self.height / self.lineCount))
+    local barSize = math.ceil(self.height * self.height / self.lineCount)
+    if barSize == self.height and barSize < self.lineCount then
+        barSize = self.height - 1
+    end
 
     local barPos = math.ceil(self.height * topline / self.lineCount)
     if barPos + barSize > self.height then