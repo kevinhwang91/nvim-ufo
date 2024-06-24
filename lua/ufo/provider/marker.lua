1d612f8bedf8bf4147e08b5b869478b84717dc2d
LucasAVasco <67849851+LucasAVasco@users.noreply.github.com>
2024-06-24 00:22:54 -0300

    refactor(provider)!: VS Code region marker kind
    Change the kind applied to a VS Code region folding. Before this commit,
    the applied kind was 'marker'. Now it is 'region'
    
    Do not change the kind applied to Neovim markers

--- lua/ufo/provider/marker.lua
+++ lua/ufo/provider/marker.lua
@@ -15,15 +15,17 @@ function Marker.getFolds(bufnr)
     local buf = bufmanager:get(bufnr)
     local winid = utils.getWinByBuf(bufnr)
 
-    -- Defines the 'start' and 'end' markers that the provider will search. Each element of the `markers` list is a pair of the 'start'
-    -- and 'end' marker. Example: `local markers = { { 'start marker', 'end marker' } }`
-    -- The search is done by marker pair. One marker pair does not affect the other. So the end marker of `markers[0]` will not close the start
-    -- marker of `markers[1]`, by example
+    -- Defines the 'start' and 'end' markers that the provider will search, and the kind to apply
+    -- to these markers. Each element of the `markers` list is a list of the 'start', 'end' markers
+    -- and kind applied, in this order. Example: `local markers = { { 'start marker', 'end marker', 'marker kind' } }`
+    -- The search is done by marker pair. One marker pair does not affect the other. So the end marker of `markers[0]`
+    -- will not close the start marker of `markers[1]`, by example
     local markers = {
-        vim.fn.split(vim.wo[winid].foldmarker, ','),  -- Configured Vim marker
+        vim.fn.split(vim.wo[winid].foldmarker .. ',marker', ','),  -- Configured Vim marker
         {
-            '#region ',   -- Start of VS code marker
-            '#endregion'  -- End of VS Code marker
+            '#region ',    -- Start of VS code marker
+            '#endregion',  -- End of VS Code marker
+            'region',      -- Kind to be applied to a VS Code region folding
         }
     }
 
@@ -56,7 +58,7 @@ function Marker.getFolds(bufnr)
                     if relatedOpenMarkerLine then
                         table.insert(
                         folds,
-                        foldingrange.new(relatedOpenMarkerLine - 1, lineNum - 1, nil, nil, 'marker')
+                        foldingrange.new(relatedOpenMarkerLine - 1, lineNum - 1, nil, nil, marker[3])
                         )
                     end
                 end
@@ -65,7 +67,7 @@ function Marker.getFolds(bufnr)
 
         -- Closes all remaining open markers (they will be open to the end of the file)
         for _, markerStart in ipairs(openMarkerLines) do
-            table.insert(folds, foldingrange.new(markerStart - 1, #lines, nil, nil, 'marker'))
+            table.insert(folds, foldingrange.new(markerStart - 1, #lines, nil, nil, marker[3]))
         end
     end
 
